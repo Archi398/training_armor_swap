37cdf419876e5db980fe5b197c488515887a2053
Archi <archisabatier@gmail.com>
2022-01-20 22:30:33 +0100

    version stable

--- js/script.js
+++ js/script.js
@@ -2,19 +2,47 @@ var pressed;
 
 var fired = false;
 var endPress = false;
+var swapSuccess = false;
+var start = false;
+
+var timer;
+
+var elkeyE = document.getElementById('keyE');
+
+elkeyE.requestPointerLock = elkeyE.requestPointerLock || elkeyE.mozRequestPointerLock;
+
+elkeyE.onclick = function() {
+    elkeyE.requestPointerLock();
+    start = true;
+};
 
 $("body").on('keydown', function(event) {
-    if (event.code === "KeyE" && !fired) {
+    // console.log(event.code);
+    if (event.code === "KeyE" && !fired && start) {
         pressed = setTimeout(function() {
-            console.log('1sec');
+            $('#loot_box_div').show();
+            timer = Date.now();
+            document.exitPointerLock();
             endPress = true;
-        }, 1000);
-        document.getElementById('progressCircle').style.animation="offsettozero 1s linear forwards";
+        }, 500);
+        document.getElementById('progressCircle').style.animation="offsettozero 0.5s linear forwards";
         fired = true;
     }
     if (event.code === "KeyR") {
         clearTimeout(pressed);
+        $('#loot_box_div').hide();
+        document.getElementById('progressCircle').style.animation="";
         fired = false;
+        endPress = false;
+        $('#armorValid').hide();
+        swapSuccess = false;
+        $('#result').hide();
+        start = false;
+    }
+    if (event.code === "Escape" && swapSuccess) {
+        var delta = ((Date.now() - timer) / 1000);
+        document.getElementById("timer").innerHTML = delta;
+        $('#result').show();
     }
 });
 
@@ -23,5 +51,13 @@ $("body").on('keyup', function(event) {
         clearTimeout(pressed);
         document.getElementById('progressCircle').style.animation="";
         fired = false;
+        endPress = false;
+        $('#armorValid').hide();
     }
+});
+
+$("#armorInvald").on('click', function(event) {
+    $('#armorValid').show();
+    swapSuccess = true;
+    event.preventDefault();
 });
